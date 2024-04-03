1adce67f69a5bd28fbd4e68790200fb5302443d1
Daniel Miranda Alejandro <Danielmiranda@sanluis.edu.ar>
2024-04-02 23:48:20 -0300

    CC

---
+++ Dabi/src/main/java/com/mycompany/dabi/Dabi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dabi;
+
+/**
+ *
+ * @author DANIELALEJANDROMIRAN
+ */
+public class Dabi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
